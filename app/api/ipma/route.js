5d6f52bfd0950799c76349bfe5df27eb14cd5501
martim-lusofona <martim.mourao@ulusofona.pt>
2024-09-27 17:11:47 +0100

    Added cache reset on ipma API request

--- app/api/ipma/route.js
+++ app/api/ipma/route.js
@@ -1,6 +1,6 @@
 export async function GET() {
-    const resp = await fetch(`https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1010500.json`)
+    const resp = await fetch(`https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1010500.json`, { cache: "no-cache" })
     const data = await resp.json()
-    
-    return  Response.json({data})
+
+    return Response.json({ data })
 }
