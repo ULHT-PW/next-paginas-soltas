21763bfe625e3d63eeb4b5b13ff8acf3eec1f081
martim-lusofona <martim.mourao@ulusofona.pt>
2024-09-27 17:39:37 +0100

    Fix next build

--- app/api/example/route.js
+++
@@ -1,7 +0,0 @@
-
-export async function GET() {
-    const resp = await fetch(`https://dummy.restapiexample.com/api/v1/employees`)
-    const data = await resp.json()
-    
-    return Response.json({data});
-}
